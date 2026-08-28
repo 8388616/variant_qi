58195a8da4735ad4066b400defc6facd1118f988
8388616 <8388616@gmail.com>
2026-08-28 08:50:21 +0800

    .

--- katagos/biliberty-weiqi/cpp/external/katagocoreml/include/katagocoreml/Version.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace katagocoreml {
-constexpr const char* VERSION = "1.1.0";
-constexpr int VERSION_MAJOR = 1;
-constexpr int VERSION_MINOR = 1;
-constexpr int VERSION_PATCH = 0;
-}
